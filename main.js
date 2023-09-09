c09b91a18583c3a251f5dbf67d15989723ee40ae
Hrishabh963 <hrishabhkumar634@gmail.com>
2023-09-09 11:02:34 +0530

    Updated main.js

--- main.js
+++ main.js
@@ -26,6 +26,8 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end(`Success after ${delay} seconds of delay`);
       }, delay * 1000);
+    } else {
+      res.end(`Invalid endpoint`);
     }
   }
 });
