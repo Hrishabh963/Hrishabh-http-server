fba36d1fc5892914bd2c2a060d40b189162646f4
Hrishabh963 <hrishabhkumar634@gmail.com>
2023-09-09 10:42:49 +0530

    Completed HTTP_Server drill
    Successfully completed http server drill using HTTP core node js module

---
+++ data.js
@@ -0,0 +1,32 @@
+module.exports.data = {
+  slideshow: {
+    author: 'Yours Truly',
+    date: 'date of publication',
+    slides: [
+      {
+        title: 'Wake up to WonderWidgets!',
+        type: 'all',
+      },
+      {
+        items: [
+          'Why <em>WonderWidgets</em> are great',
+          'Who <em>buys</em> WonderWidgets',
+        ],
+        title: 'Overview',
+        type: 'all',
+      },
+    ],
+    title: 'Sample Slide Show',
+  },
+};
+
+module.exports.html = `<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+      <h1>Any fool can write code that a computer can understand. Good programmers write code that humans can understand.</h1>
+      <p> - Martin Fowler</p>
+
+  </body>
+</html>`;
---
+++ main.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const jsonData = require('./data').data;
+const getUuid = require('./getUuid');
+const htmlCode = require('./data').html;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/html') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(htmlCode);
+  } else if (req.url === '/json') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(jsonData));
+  } else if (req.url === '/uuid') {
+    const uuid = getUuid();
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ uuid: uuid }));
+  } else {
+    const pathParams = req.url.split('/');
+    if (pathParams[1] === 'status') {
+      res.writeHead(Number(pathParams[2]), { 'Content-Type': 'text/plain' });
+      res.end(`Ending the request with status ${pathParams[2]}`);
+    }
+    if (pathParams[1] === 'delay') {
+      const delay = Number(pathParams[2]);
+      setTimeout(async () => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`Success after ${delay} seconds of delay`);
+      }, delay * 1000);
+    }
+  }
+});
+server.listen('3000');
