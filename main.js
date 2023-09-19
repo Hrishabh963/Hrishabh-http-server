1390dea1e8376db832ae808e494636255174aa79
Hrishabh963 <hrishabhkumar634@gmail.com>
2023-09-19 11:22:40 +0530

    Changed main.js

--- main.js
+++ main.js
@@ -35,4 +35,10 @@ const server = http.createServer((req, res) => {
     }
   }
 });
-server.listen('3000');
+server.listen('3000', (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`Listening on port 3000`);
+  }
+});
